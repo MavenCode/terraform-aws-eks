84ab14978605c71be90a05bc3b44c996dd46fcec
Ian MacDonald <38594415+en-mac@users.noreply.github.com>
2022-04-23 17:28:51 -0500

    upgrade cluster version (#2)

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,6 +1,7 @@
 resource "aws_eks_cluster" "eks" {
   name     = var.eks_cluster_name
   role_arn = aws_iam_role.eks.arn
+  version  = var.kubernetes_version
 
   vpc_config {
     security_group_ids      = [aws_security_group.eks_cluster.id]
